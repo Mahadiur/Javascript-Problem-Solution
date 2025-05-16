23d2d88dae779f559b11457db579d6809084b2f1
Mahadiur <almahadiurrahman@gmail.com>
2025-05-16 16:42:43 +0400

    Problem9 is Solved

---
+++ Problem-9/solution9.js
@@ -0,0 +1,15 @@
+// Question array
+let my_array = [4, 8, 7, 4,3,6,2,1,9];
+
+// Function & loop
+function looparray(array1){
+    for(let i=0; i < array1.length;i++){
+        if (array1[i] == 6 ){
+            console.log('True')
+        }
+    }    
+}
+
+
+// function call
+looparray(my_array)
