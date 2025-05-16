3bc3f0f86fb70832d19b3366f721c0fbfbeab3e5
Mahadiur <almahadiurrahman@gmail.com>
2025-05-16 14:28:06 +0400

    Problem3 is Solved

---
+++ Problem-3/solution3.js
@@ -0,0 +1,8 @@
+let firstAge=Number(prompt('Enter First Age:'));
+let secondAge=Number(prompt('Enter Second Age:'));
+
+if (firstAge > secondAge){
+    console.log(`Elder Brother age is ${firstAge}`);
+}else{
+    console.log(`Elder Brother age is ${secondAge}`);
+}
